b83166a00f09b31aded8def3dc93e7cf3bfe2617
Hirva Dhandhukia <67074147+hirvaDhandhukia@users.noreply.github.com>
2021-11-09 16:37:08 +0530

    update: condole logged all the todos you can see all the todos in the console as you input them

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
       <h1>To-Do!</h1>
-      <TodoForm />
+      <TodoList />
     </div>
   );
 }
