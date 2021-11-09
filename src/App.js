67d23ff8282fe43909bd803d6274f4998ee77d14
Hirva Dhandhukia <67074147+hirvaDhandhukia@users.noreply.github.com>
2021-11-09 16:21:31 +0530

    add: input form user can input task and click the submit btn, the page won't get refresh every time.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import TodoForm from "./components/TodoForm";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
       <h1>To-Do!</h1>
-      <p>Get your work done with this todo- web app</p>
+      <TodoForm />
     </div>
   );
 }
